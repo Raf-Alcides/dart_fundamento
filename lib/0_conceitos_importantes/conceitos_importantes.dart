1e0c2875f410b29dff9d4e97669e4223af31ed60
Rafael Alcides <devrafalcides@gmail.com>
2023-03-12 20:22:23 -0300

    Conceitos importantes

---
+++ lib/0_conceitos_importantes/conceitos_importantes.dart
@@ -0,0 +1,19 @@
+//O dart permite funções e variaveis de  nivel superior
+String nomeCompleto = 'Rafael Alcides'; //variavel de nivel superior
+void main() {
+  // Tudo dentro do dart é um objeto
+  // Objeto são classes
+  
+  String nome = 'Rafael';
+  print(nome);
+  print(nomeCompleto);
+  print(Rafael);
+  // 
+}
+
+// Funcao de nivel superior
+class Rafael {
+   String name(String nome) {
+    return 'Rafael';
+   }
+}
