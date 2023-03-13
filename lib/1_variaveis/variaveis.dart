dc1fae38598f7d8cccae194fb04c4fa70e1c66d8
Rafael Alcides <devrafalcides@gmail.com>
2023-03-12 21:09:36 -0300

    Aprendendo Sobre Variaveis

---
+++ lib/1_variaveis/variaveis.dart
@@ -0,0 +1,35 @@
+//fuanção de nivel superior
+void main() {
+  //Variaveis são pequenos espaços na memoria do computador usado pra guarda valores
+
+  //3 tipos basicos de variaveis
+  //numerais
+  //textos
+  //boleanos
+
+  //criação de variaveis
+  //tipo de variaveis
+  //nome da variavel
+  //atribuicao de valores
+  //ex
+  int idade = 23;  // int herda da classe num
+  double peso = 75.0; // int herda da classe num
+  num qualquerNumero = 14;
+  String nome = 'Rafael';
+  bool eUmaPessoa = true;  //camel-case
+
+  // Podemos definir um tipo usando inferencia de tipo
+  var inferenciaTipo = 'Rafael';
+
+  // Todas as classes herdam de object
+  // evitar ao maximo a ultitização
+  Object simNao = true;
+  Object nomeQualquer = 'Rafael';
+
+  // O tipo dymanic pode receber qualquer tipo
+  // evitar ao maximo a ultitização
+  Object simOuNao = true;
+  Object qualquer ;
+  qualquer = 'rafael';
+  qualquer = 'Alcides';
+}
